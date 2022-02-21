2f8ff90e98ab988c722cdaabd90130ce337470e1
oriol.sanchez.7e4 <oriol.sanchez.7e4@itb.cat>
2022-02-21 17:18:06 +0100

    e

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./App.js";
-import Profile from "./Account_nomod.jsx"
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Home";
+import Main from "./Main";
 import Footer from "./Footer";
 import Blog from "./Blog";
 import Navigation from "./Navigation";
+import Account from './Account';
+import Home from './App';
 
 ReactDOM.render(
   <Router>
     <Navigation />
     <Routes>
-      <Route path="/login" element={<Profile />} />   
+      <Route path='/' element={<Main />} />
+      <Route path="/login" element={<Home />} />   
       <Route path="/blog" element={<Blog />}/>  
-      <Route path="/home" element={<Home />}>
+      <Route path="/home" element={<Main />}>
       </Route>
     </Routes>
     <Footer />
